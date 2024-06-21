44d68cc1e6a19131d915cf844e85bcc3ddee7dc5
Mark Widman <mark@pieces.app>
2024-06-21 18:51:40 -0400

    - chore: update our widget to be a Future builder

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,32 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  String greeting() {
+    return 'Action: Call Rust `greet("Mark")`\nResult: `${greet(name: "Mark")}`';
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
         body: Center(
-          child: Text('Action: Call Rust `greet("Mark")`\nResult: `${greet(name: "Mark")}`'),
+          child: FutureBuilder<String>(
+            future: (() async {
+              String _greeting = greeting();
+              // TODO add our stringLength
+              return '$_greeting: (??? chars)';
+            })(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return Text(snapshot.data ?? 'No data');
+              }
+            },
+          ),
         ),
       ),
     );
